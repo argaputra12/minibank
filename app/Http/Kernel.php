f166bd786f83741dc8a4eb096f57151391d85b26
argaputra12 <argaputra@student.uns.ac.id>
2022-07-11 12:19:31 +0700

    add teller, cs, admin, user

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\Admin::class,
+        'cs' => \App\Http\Middleware\CS::class,
+        'teller' => \App\Http\Middleware\Teller::class,
     ];
 }
